60ef0e40d7810e610926b8116644437112ea1c8c
Vojtěch Döme <doev@alz249460aa.local>
2025-03-14 10:35:01 +0100

    feat: 0.5

--- components/custom/navbar.tsx
+++ components/custom/navbar.tsx
@@ -26,25 +26,34 @@ export const Navbar = () => {
   return (
     <>
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="px-8 flex h-16 items-center justify-between">
-          <Link href="/" className="flex items-center gap-2 font-bold text-xl">
-            <Scale className="h-6 w-6" />
-            <span>AK</span>
-          </Link>
-          <nav className="hidden md:flex gap-6">
-            {navbarItems.map((item, index) => (
-              <Link
-                className="hover:scale-105 transition-all duration-200"
-                key={index}
-                href={item.href}
-              >
-                <p>{item.name}</p>
-              </Link>
-            ))}
-          </nav>
-          <Button asChild className="hidden md:inline-flex">
-            <Link href="#kontakty">Domluvit konzultaci</Link>
-          </Button>
+        <div className="w-full px-8 flex h-16 items-center ">
+          <div className="w-1/3">
+            <Link
+              href="/"
+              className="flex items-center gap-2 font-bold text-xl"
+            >
+              <Scale className="h-6 w-6" />
+              <span>AK</span>
+            </Link>
+          </div>
+          <div className="w-1/3 flex justify-center">
+            <nav className="hidden md:flex gap-6">
+              {navbarItems.map((item, index) => (
+                <Link
+                  className="hover:scale-105 transition-all duration-200"
+                  key={index}
+                  href={item.href}
+                >
+                  <p>{item.name}</p>
+                </Link>
+              ))}
+            </nav>
+          </div>
+          <div className="w-1/3 flex justify-end">
+            <Button asChild className="hidden md:inline-flex">
+              <Link href="#kontakty">Domluvit konzultaci</Link>
+            </Button>
+          </div>
           <Sheet>
             <SheetTrigger asChild>
               <Button variant="outline" size="icon" className="md:hidden">
--- components/custom/section.tsx
+++ components/custom/section.tsx
@@ -1,14 +1,16 @@
 export const Section = ({
   children,
   id,
+  className,
 }: {
   children: React.ReactNode;
   id: string;
+  className?: string;
 }) => {
   return (
     <section
       id={id}
-      className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-muted min-h-screen"
+      className={`w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-muted min-h-[50vh] lg:min-h-screen ${className}`}
     >
       {children}
     </section>
