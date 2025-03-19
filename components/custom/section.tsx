684a40c6b0d82b6a4db4fd25d68865b0c9081493
Vojtěch Döme <doev@alz249460aa.local>
2025-03-19 14:55:00 +0100

    fix: 0.54

--- components/custom/section.tsx
+++ components/custom/section.tsx
@@ -10,7 +10,7 @@ export const Section = ({
   return (
     <section
       id={id}
-      className={`w-full bg-muted min-h-[90vh] lg:justify-center flex flex-col first:pt-12 pt-24 items-center  ${className}`}
+      className={`w-full bg-muted min-h-[90vh] lg:justify-center flex flex-col first:pt-8 pt-24 items-center  ${className}`}
     >
       {children}
     </section>
