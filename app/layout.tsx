852bd275361428021ea053cdf376a1d839e952c4
Vojtěch Döme <doev@eduroam151-246.nbk.vse.cz>
2025-03-24 12:07:43 +0100

    fix: 0.611

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/theme-provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Advokátní kancelář - Kateřina Trnkova",
@@ -13,8 +24,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html style={{ scrollBehavior: "smooth" }} lang="en">
-      <body className={`antialiased`}>
+    <html className="scroll-smooth" lang="cs">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
