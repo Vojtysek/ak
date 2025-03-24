989e47189de3c190250d10a0836dc35e290765d5
Vojtěch Döme <doev@eduroam151-246.nbk.vse.cz>
2025-03-24 12:58:45 +0100

    fix: 0.63

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,47 +1,22 @@
-"use client"; // Add this line at the top
+"use client";
 
-import { ThemeProvider } from "next-themes";
-import Head from "next/head";
-import { useEffect, useState } from "react";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return (
-      <html lang="en">
-        <body></body>
-      </html>
-    );
-  }
-
+}>) {
   return (
-    <html lang="cs">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Geist:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body className="antialiased">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+    <html lang="cs" className="scroll-smooth">
+      <body className={`antialiased ${inter.className}`}>{children}</body>
     </html>
   );
 }
